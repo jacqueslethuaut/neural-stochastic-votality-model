f991c7c8ded434af7bf11592e8616335d896e5ee
jacqueslethuaut <jacques.lethuaut@gmail.com>
2023-06-08 19:28:49 +0200

    add dropout & is_training param to not enable it

--- neural_network.py
+++ neural_network.py
@@ -8,10 +8,22 @@ File: arch_model.py
 import numpy as np
 
 class NeuralNetwork:
-    def __init__(self, input_size, hidden_size, output_size, initialization='xavier', lambda_=0.01, regularization='l2'):
+    def __init__(self, 
+                 input_size, 
+                 hidden_size, 
+                 output_size, 
+                 initialization='xavier', 
+                 lambda_=0.01, 
+                 regularization='l2',
+                 dropout_rate=0.5):
+        
         self.initialization = initialization
+        # regularization 
         self.lambda_ = lambda_  # regularization coefficient
         self.regularization = regularization
+        # dropout
+        self.dropout_rate = dropout_rate
+        
         # Initialize weights and biases
         if self.initialization == 'xavier':
             self.W1 = np.random.randn(input_size, hidden_size) / np.sqrt(input_size)
@@ -32,15 +44,22 @@ class NeuralNetwork:
     def sigmoid_derivative(self, z):
         return self.sigmoid(z) * (1 - self.sigmoid(z))
 
-    def forward_pass(self, X):
+    def forward_pass(self, X, is_training=True):
         # Compute hidden layer
         Z1 = np.dot(X, self.W1) + self.b1
         A1 = self.sigmoid(Z1)
+        
+        if is_training:
+            # Dropout
+            self.drop1 = (np.random.rand(*A1.shape) > self.dropout_rate)
+            A1 *= self.drop1
+        else:
+            A1 *= (1 - self.dropout_rate)
 
         # Compute output layer
         Z2 = np.dot(A1, self.W2) + self.b2
         A2 = self.sigmoid(Z2)
-
+        
         self.A1 = A1
         self.A2 = A2
 
@@ -50,10 +69,12 @@ class NeuralNetwork:
         m = X.shape[0]
 
         dZ2 = self.A2 - y
-        dW2 = (1 / m) * np.dot(self.A1.T, dZ2) 
+        dW2 = (1 / m) * np.dot(self.A1.T, dZ2)
         db2 = (1 / m) * np.sum(dZ2, axis=0, keepdims=True)
 
-        dZ1 = np.dot(dZ2, self.W2.T) * self.sigmoid_derivative(self.A1)
+        dA1 = np.dot(dZ2, self.W2.T)
+        dZ1 = dA1 * self.sigmoid_derivative(self.A1)
+        dZ1 *= self.drop1  # Apply dropout
         dW1 = (1 / m) * np.dot(X.T, dZ1)
         db1 = (1 / m) * np.sum(dZ1, axis=0, keepdims=True)
 
